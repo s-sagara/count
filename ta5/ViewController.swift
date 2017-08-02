6be58847d47c7f72d19a4baaf9ba3e769c756d0e
nttr <nttr@BI-23557.local>
2017-08-02 14:27:18 +0900

    add counter funcs @ viewcontroller & change mainstoryboard

--- ta5/ViewController.swift
+++ ta5/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number:Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+    
+        number += 1
+        label.text = String(number)
+    }
 
+    
+    @IBAction func minus(){
+        
+        number -= 1
+        label.text = String(number)
+    }
+    
+    
+    @IBAction func clear(){
+        
+        number = 0
+        label.text = String(number)
+    }
+    
+    
 }
 
